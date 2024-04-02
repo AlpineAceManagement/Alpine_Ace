5fb8a2936c57021a636565841678cfbc823ca3bc
AJPB4133 <andrea.jeanpaul99@gmail.com>
2024-04-02 09:42:11 +0200

    snowapi-v2.1
    logging function inplemented

--- API/snow_api_v2.py
+++ API/snow_api_v2.py
@@ -1,40 +1,53 @@
+'''import relevant libraries'''
 import requests
 import psycopg2
 import json
 import time
+import logging
 
 # Database credentials
 db_config = {
-    'host': 'localhost',
-    'database': 'AlpineACE',
-    'user': 'postgres',
-    'password': 'TeamLH44'
+    'host': 'localhost', #Hostname der DB
+    'database': 'AlpineACE', #Name der DB
+    'user': 'postgres', #Username für die Verbindung zur DB
+    'password': 'TeamLH44'  #Passwort für den Usernamen
 }
 
+# Configure logging
+
+logging.basicConfig(filename="snow_measuremnts.log",
+                    level=logging.INFO,
+                    format="%(asctime)s - %(levelname)s - %(message)s")
+
+#  Connect to the database
 def fetch_and_store_measurement(station_code):
     api_url = f"https://measurement-api.slf.ch/public/api/imis/station/{station_code}/measurements"
+    try:
+        response = requests.get(api_url)
+        response.raise_for_status()
 
-    response = requests.get(api_url)
-    response.raise_for_status()
+        data = response.json()  
+        item= len(data)
+        # Extract the last measurement
+        last_measurement = data[item -1]  # Zero-based indexing
 
-    data = response.json()  
-    item= len(data)
-    # Extract the second measurement
-    second_measurement = data[item -1]  # Zero-based indexing
-
-    with psycopg2.connect(**db_config) as conn:
-        with conn.cursor() as cursor:
-            sql = """
-                INSERT INTO schneehoehe (station_code, sh_zeit, sh_hoehe)
-                VALUES (%s, %s, %s)
-            """
-            values = (
-                second_measurement['station_code'], 
-                second_measurement['measure_date'], 
-                second_measurement['HS']
-            )
-            cursor.execute(sql, values)
-
+        with psycopg2.connect(**db_config) as conn:
+            with conn.cursor() as cursor:
+                sql = """
+                    INSERT INTO schneehoehe (station_code, sh_zeit, sh_hoehe)
+                    VALUES (%s, %s, %s)
+                """
+                values = (
+                    last_measurement['station_code'], 
+                    last_measurement['measure_date'], 
+                    last_measurement['HS']
+                )
+                cursor.execute(sql, values)
+        logging.info(f"Measurement fetched and stored for station: {station_code}")
+    except requests.exceptions.RequestException as e:
+        logging.error(f"Request error for station {station_code}: {e}")
+    except psycopg2.Error as e:
+        logging.error(f"Database error for station {station_code}: {e}")
 if __name__ == '__main__':
     station_codes = ["ROT3", "CMA2", "GOS3"] 
      
