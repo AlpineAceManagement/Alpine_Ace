51842ed5dd7338b1e1a3773d971c26e33cd0fd3a
AJPB4133 <andrea.jeanpaul99@gmail.com>
2024-04-17 21:25:38 +0200

    Update database configuration variables with comments translated to English
    Changes include:
    
    * 'Hostname der DB' to 'Hostname of DB'
    * 'Port der DB' to 'Port of DB'
    * 'Name der DB'

--- API/config.py
+++ API/config.py
@@ -1,9 +1,9 @@
 '''this code is for the connection with the Postgres Database'''
 #Database credentials
 db_config = {
-    'host': 'localhost', #Hostname der DB
-    'port': '5432', #Port der DB
-    'database': 'AlpineACE', #Name der DB
-    'user': 'postgres', #Username für die Verbindung zur DB
-    'password': 'TeamLH44'  #Passwort für den Usernamen
+    'host': 'localhost', #Hostname of DB
+    'port': '5432', #Port of DB
+    'database': 'AlpineACE', #Name fo DB
+    'user': 'postgres', #Username for connection to DB
+    'password': 'TeamLH44'  #Passwort for usernamen
 }
