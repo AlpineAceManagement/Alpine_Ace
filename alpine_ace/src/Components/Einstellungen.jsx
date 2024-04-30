c0188e7229f1299186ee964b3bb16ffaab3b7f6d
AlpineAceManagement <162140178+AlpineAceManagement@users.noreply.github.com>
2024-04-30 16:21:40 +0200

    Delete alpine_ace directory

--- alpine_ace/src/Components/Einstellungen.jsx
+++
@@ -1,5 +0,0 @@
-const Einstellungen = () => {
-  return <h1>Einstellungen</h1>;
-};
-
-export default Einstellungen;
--- alpine_ace/src/Components/licences.jsx
+++
@@ -1,4 +0,0 @@
-<div>
-  Icons made from <a href="https://www.onlinewebfonts.com/icon">svg icons</a>is
-  licensed by CC BY 4.0
-</div>;
