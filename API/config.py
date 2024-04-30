f89b324234fdc9767709753d62cd080401cac34c
AlpineAceManagement <162140178+AlpineAceManagement@users.noreply.github.com>
2024-04-30 16:21:05 +0200

    Delete API directory

--- API/config.py
+++
@@ -1,9 +0,0 @@
-'''this code is for the connection with the Postgres Database'''
-#Database credentials
-db_config = {
-    'host': 'localhost', #Hostname of DB
-    'port': '5433', #Port of DB
-    'database': 'geoserver', #Name fo DB
-    'user': 'postgres', #Username for connection to DB
-    'password': 'jNtd2C13ka9oaPpRy1jP'  #Passwort for usernamen
-}
