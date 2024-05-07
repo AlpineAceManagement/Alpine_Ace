a9ba5307e4d31182f2432862620b50a067917d5c
AJPB4133 <andrea.jeanpaul99@gmail.com>
2024-05-07 16:51:09 +0200

    reset repo

--- API/config_template.py
+++
@@ -1,9 +0,0 @@
-'''this code is for the connection with the Postgres Database'''
-#Database credentials
-db_config = {
-    'host': 'host', #Hostname of DB
-    'port': 'port', #Port of DB
-    'database': 'database', #Name fo DB
-    'user': 'user', #Username for connection to DB
-    'password': 'password'  #Passwort for usernamen
-}
