bf3a31c20b28c14bb2a9d7aad1c5f0baaa5029e0
loopercamera <fabian.gross@students.fhnw.ch>
2024-05-29 08:44:41 +0200

    py install hochladen

--- alpine_ace/src/Components/Network/network_config.js
+++ alpine_ace/src/Components/Network/network_config.js
@@ -1,7 +1,7 @@
 // IP Adresse auf dem das Projekt läuft (Laptop oder Raspberry Pi)
 // Die IP Adresse wird benötigt, um die Verbindung zwischen dem Frontend und dem Backend herzustellen (WFS Abfragen und Express API)
 
-const IP = "192.168.1.111"; // hier IP Adresse eintragen
+const IP = "10.175.23.145"; // hier IP Adresse eintragen
 
 const config = {
   projectIPadress: `http://${IP}`,
